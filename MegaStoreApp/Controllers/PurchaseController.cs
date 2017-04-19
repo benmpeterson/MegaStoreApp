ae2c8476731b86ac40c84c42d478c985a4ba23ad
Ben Peterson <ben.micah.peterson@gmail.com>
2017-04-19 14:41:35 -0400

    Added Purchase Scaffold

---
+++ MegaStoreApp/Controllers/PurchaseController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MegaStoreApp.DAL;
+using MegaStoreApp.Models;
+
+namespace MegaStoreApp.Controllers
+{
+    public class PurchaseController : Controller
+    {
+        private StoreContext db = new StoreContext();
+
+        // GET: Purchase
+        public ActionResult Index()
+        {
+            var purchases = db.Purchases.Include(p => p.Album).Include(p => p.Customer);
+            return View(purchases.ToList());
+        }
+
+        // GET: Purchase/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Purchases purchases = db.Purchases.Find(id);
+            if (purchases == null)
+            {
+                return HttpNotFound();
+            }
+            return View(purchases);
+        }
+
+        // GET: Purchase/Create
+        public ActionResult Create()
+        {
+            ViewBag.AlbumID = new SelectList(db.Albums, "AlbumID", "Artist");
+            ViewBag.CustomerID = new SelectList(db.Customers, "CustomerID", "LastName");
+            return View();
+        }
+
+        // POST: Purchase/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "PurchasesID,AlbumID,CustomerID,Rating")] Purchases purchases)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Purchases.Add(purchases);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.AlbumID = new SelectList(db.Albums, "AlbumID", "Artist", purchases.AlbumID);
+            ViewBag.CustomerID = new SelectList(db.Customers, "CustomerID", "LastName", purchases.CustomerID);
+            return View(purchases);
+        }
+
+        // GET: Purchase/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Purchases purchases = db.Purchases.Find(id);
+            if (purchases == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.AlbumID = new SelectList(db.Albums, "AlbumID", "Artist", purchases.AlbumID);
+            ViewBag.CustomerID = new SelectList(db.Customers, "CustomerID", "LastName", purchases.CustomerID);
+            return View(purchases);
+        }
+
+        // POST: Purchase/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "PurchasesID,AlbumID,CustomerID,Rating")] Purchases purchases)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(purchases).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.AlbumID = new SelectList(db.Albums, "AlbumID", "Artist", purchases.AlbumID);
+            ViewBag.CustomerID = new SelectList(db.Customers, "CustomerID", "LastName", purchases.CustomerID);
+            return View(purchases);
+        }
+
+        // GET: Purchase/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Purchases purchases = db.Purchases.Find(id);
+            if (purchases == null)
+            {
+                return HttpNotFound();
+            }
+            return View(purchases);
+        }
+
+        // POST: Purchase/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Purchases purchases = db.Purchases.Find(id);
+            db.Purchases.Remove(purchases);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MegaStoreApp/ViewModel/EmployeeIndexData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MegaStoreApp.Models;
+
+namespace MegaStoreApp.ViewModel
+{
+    public class EmployeeIndexData
+    {
+        public IList<Employee> Instructors { get; set; }
+        public IEnumerable<Album> Albums { get; set; }
+        public IEnumerable<Purchases> Purchases { get; set; }
+    }
+}
