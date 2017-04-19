d4ed7b57ffda15f714b251a007f1eba0583102bc
Ben Peterson <ben.micah.peterson@gmail.com>
2017-04-19 16:16:16 -0400

    Added BootStrap Styling

---
+++ MegaStoreApp/ViewModels/AssignedCourseData.cs
@@ -0,0 +1,9 @@
+﻿namespace MegaStoreApp.ViewModels
+{
+    public class AssignedCourseData
+    {
+        public int AlbumID { get; set; }
+        public string Title { get; set; }
+        public bool Assigned { get; set; }
+    }
+}
